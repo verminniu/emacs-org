783daf9f1b004ac3547c84068c09f1aa56b54784
vermin <cattlenzq@163.com>
2025-06-17 23:47:23 +0800

    add early init

---
+++ early-init.el
@@ -0,0 +1,9 @@
+;; Example Elpaca early-init.el -*- lexical-binding: t; -*-
+
+(setq package-enable-at-startup nil)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
